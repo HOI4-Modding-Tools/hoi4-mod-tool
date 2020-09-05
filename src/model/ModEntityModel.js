d8fcc48f3c3bc42cb7bf781ceb460799b5595f2d
Damie <damienmarble@gmail.com>
2020-09-05 01:22:50 -0400

    Removed generated files in model directory.

--- src/model/ModEntityModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ModEntityModel = /** @class */ (function () {
-    function ModEntityModel() {
-    }
-    return ModEntityModel;
-}());
-exports.default = ModEntityModel;
-//# sourceMappingURL=ModEntityModel.js.map
