7b92e11d5d7868485bef1542a7a189ee345609f4
Damie <damienmarble@gmail.com>
2020-09-05 12:32:18 -0400

    Redesigned SelectLocalMod component.
    Resolves #5.

--- src/components/SelectLocalMod.jsx
+++ src/components/SelectLocalMod.jsx
@@ -1,24 +1,43 @@
 import {connect} from 'react-redux';
 import React from "react";
-import { Link } from "react-router-dom";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+// import withRouter from "react-router/modules/withRouter";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
 
 export class SelectLocalMod extends React.Component {
+    constructor(props) {
+        super(props);
+        this.routeToUrl = (url) => {
+            return () => {
+                console.log("Routing to", url);
+                this.props.history.push(url);
+            }
+        }
+    }
+
 
     render() {
         const list = Object.keys(this.props.mods).map(mod => {
             const linkTo = "/mod/" + encodeURIComponent(this.props.mods[mod]._descriptor.name);
-            return (<li key={this.props.mods[mod]._descriptor.location}><Link to={linkTo}>{this.props.mods[mod]._descriptor.name}</Link></li>)
+            return (
+                <ListItem key={this.props.mods[mod]._descriptor.location} button onClick={this.routeToUrl(linkTo)}>
+                    <ListItemText align="center">
+                        {this.props.mods[mod]._descriptor.name}
+                    </ListItemText>
+                </ListItem>)
         });
-        if(list.length) {
+        if (list.length) {
             return (
                 <div>
-                    <div>
+                    <h1 style={{textAlign: "center"}}>
                         Available Local Mods
-                    </div>
-                    <ul>
+                    </h1>
+                    <List>
                         {list}
-                    </ul>
-                    <div>To create new mods, use the built-in tools in the Paradox Launcher.</div>
+                    </List>
+                    <div style={{textAlign: "center"}}>To create new mods, use the built-in tools in the Paradox Launcher.</div>
                 </div>)
         } else {
             return (
@@ -38,8 +57,6 @@ const connected = connect((state, ownProps) => {
     return {
         mods: state.mods
     };
-}, (dispatch, ownProps) => {
-    return {};
-})(SelectLocalMod);
+})(/*withRouter(*/SelectLocalMod/*)*/);
 
 export default connected;
