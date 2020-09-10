bebdff5efec0b292340718bd2e281d7de05193e1
Damie <damienmarble@gmail.com>
2020-09-10 13:27:43 -0400

    Rearranged declaration to ensure category object non-null.

--- src/model/ModModel.ts
+++ src/model/ModModel.ts
@@ -4,13 +4,13 @@ import UnitModel from "./UnitModel";
 
 export default class ModModel {
     private _descriptor:ModDescriptor;
-    private readonly _equipment: {[index:string]: EquipmentModel} = {};
-    private readonly _units: {[index:string]: UnitModel} = {};
+    private readonly _equipment: {[index:string]: EquipmentModel};
+    private readonly _units: {[index:string]: UnitModel};
     public constructor(descriptor: ModDescriptor, equipment: {[index:string]: EquipmentModel},
                        units:{[index:string]: UnitModel}) {
         this._descriptor = descriptor;
-        this._equipment = equipment;
-        this._units = units;
+        this._equipment = equipment || {};
+        this._units = units || {};
     }
 
     get descriptor(){
@@ -28,23 +28,5 @@ export default class ModModel {
      get units() {
         return this._units;
      }
-
-     // asSimpleObject() {
-     //    const object: {[property:string] : any} = {};
-     //    for(const property of ["_equipment", "_units"]) {
-     //        switch (typeof this[property]) {
-     //            case "object":
-     //                object[property] = {};
-     //                for(const objectProperty in this[property]) {
-     //                    const myPropertyValue = (<any>this[property][objectProperty]);
-     //                    object[property][objectProperty] = myPropertyValue.asSimpleObject();
-     //                }
-     //                break;
-     //            default:
-     //                object[property] = this[property];
-     //        }
-     //    }
-     //    object._descriptor = this._descriptor;
-     //    return object;
-     // }
+     //
 }
