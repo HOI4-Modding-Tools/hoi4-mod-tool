3a75d36f298e7b90f44ed4b7638dd71ab4816b16
Damie <damienmarble@gmail.com>
2020-09-10 13:36:39 -0400

    Move parser generation logic into parent.

--- src/loaders/readers/EntityReader.ts
+++ src/loaders/readers/EntityReader.ts
@@ -1,6 +1,18 @@
 import "reflect-metadata";
 import ModEntityModel from "../../model/ModEntityModel";
+import {DataFileLexer} from "../parsers/DataFileLexer";
+import {DataFileParser} from "../parsers/DataFileParser";
+import * as antlr4 from "antlr4";
 
-export default interface EntityReader<T extends ModEntityModel> {
-    read(input:string):T;
+export default abstract class EntityReader<T extends ModEntityModel> {
+    abstract read(input:string):T;
+
+    protected generateTree(input: string) {
+        const inputStream = new antlr4.InputStream(input);
+        const lexer = new DataFileLexer(inputStream);
+        const tokenStream = new antlr4.CommonTokenStream(lexer);
+        const parser = new DataFileParser(tokenStream);
+        parser.buildParseTrees = true;
+        return parser.declaration();
+    }
 }
--- src/loaders/readers/EquipmentReader.ts
+++ src/loaders/readers/EquipmentReader.ts
@@ -1,18 +1,10 @@
 import EntityReader from "./EntityReader";
 import EquipmentModel from "../../model/EquipmentModel";
-import {DataFileLexer} from "../parsers/DataFileLexer";
-import {DataFileParser} from "../parsers/DataFileParser";
-import * as antlr4 from "antlr4";
 import EntityBuilder from "../builders/EntityBuilder";
 
-export default class EquipmentReader implements EntityReader<EquipmentModel> {
+export default class EquipmentReader extends EntityReader<EquipmentModel> {
     read(input: string): any {
-        const inputStream = new antlr4.InputStream(input);
-        const lexer = new DataFileLexer(inputStream);
-        const tokenStream = new antlr4.CommonTokenStream(lexer);
-        const parser = new DataFileParser(tokenStream);
-        parser.buildParseTrees = true;
-        const tree = parser.declaration();
+        const tree = this.generateTree(input);
         return {
             "equipment": new EntityBuilder(EquipmentModel).buildFrom(tree)
         };
